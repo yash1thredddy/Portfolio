5618a56834727debae5178e0324cbdd2d8b60a04
yash1thredddy <yashwanth6622@gmail.com>
2025-02-01 21:52:11 -0600

    Updated Loading

--- app/page.tsx
+++ app/page.tsx
@@ -29,14 +29,14 @@ export default function Page() {
     fetchData();
   }, []);
 
-  if (loading) {
-    return (
-      <div className='h-screen w-screen flex flex-col items-center justify-center gap-5 text-violet-600 fixed z-30 bg-gray-100 dark:bg-grey-900'>
-        <FaNodeJs size={100} className='animate-pulse' />
-        <p className='animate-pulse text-xl'>Loading...</p>
-      </div>
-    );
-  }
+if (loading) {
+  return (
+    <div className='h-screen w-screen flex flex-col items-center justify-center gap-5 text-violet-600 fixed z-30 bg-gray-100 dark:bg-grey-900'>
+      <div className='text-6xl font-bold animate-pulse'>YR</div>
+      <p className='animate-pulse text-xl'>Loading...</p>
+    </div>
+  );
+}
 
   return portfolioData ? <HomePage data={portfolioData} /> : null;
 }
