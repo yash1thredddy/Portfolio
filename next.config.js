ec224d4763da68ab1db0ba2d0367ccad2f13cb2d
yash1thredddy <yashwanth6622@gmail.com>
2025-02-01 21:47:04 -0600

    Updated Version 2

--- next.config.js
+++ next.config.js
@@ -1,29 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true
-    },
-    images: {
-        unoptimized: true,
-        domains: [
-            'res.cloudinary.com',
-            'firebasestorage.googleapis.com',
-            'img.icons8.com',
-            'raw.githubusercontent.com',
-            'i.imgur.com',
-            'drive.google.com',
-            'img.freepik.com',
-            'media.geeksforgeeks.org',
-            'hub.knime.com',
-            'upload.wikimedia.org'
-        ]
-    },
-    basePath: '/Portfolio',
-    assetPrefix: '/Portfolio/',
-    trailingSlash: true,
-    // Add these to improve static generation
-    reactStrictMode: true,
-    swcMinify: true
+  experimental: {
+    appDir: true
+  },
+  images: {
+    unoptimized: true,
+    domains: [
+      'res.cloudinary.com',
+      'firebasestorage.googleapis.com',
+      'img.icons8.com',
+      'raw.githubusercontent.com',
+      'i.imgur.com',
+      'drive.google.com',
+      'img.freepik.com',
+      'media.geeksforgeeks.org',
+      'hub.knime.com',
+      'upload.wikimedia.org'
+    ]
+  }
 };
 
 module.exports = nextConfig;
